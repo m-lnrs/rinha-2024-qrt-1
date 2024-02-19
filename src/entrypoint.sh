81a321ab4836f144026c65342c2e21e433950cba
m-lnrs <matheuslinares@outlook.com>
2024-02-19 09:55:12 -0300

    Set application timeout to zero

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -9,7 +9,7 @@
 PORT="${APP_PORT:=5000}"
 THREADS="${APP_THREADS:=1}"
 WORKERS="${APP_WORKERS:=1}"
-TIMEOUT="${APP_TIMEOUT:=30}"
+TIMEOUT="${APP_TIMEOUT:=0}"
 
 #--worker-class gevent
 exec gunicorn --workers $WORKERS --threads $THREADS --timeout $TIMEOUT -b 0.0.0.0:$PORT wsgi:app
