e2416cfe0845ea00b8eedc3aa92988fb0f0292e2
m-lnrs <matheuslinares@outlook.com>
2024-02-19 09:51:30 -0300

    Improve formatting

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,9 +1,11 @@
-# # bash parameter expansion: assign a DEFAULT value when environment variable not set or null
+# bash parameter expansion: assign a DEFAULT value when environment variable not set or null
 
-# PORT will be set to '5000' if APP_PORT not set or null. The value of APP_PORT remains not set.
+# PORT will be set to '5000' if APP_PORT not set or null
+# the value of APP_PORT remains not set
 # PORT="${APP_PORT:-5000}"
 
-# if APP_PORT not set or null, set it's value to '5000'. Then that value will be set to PORT.
+# if APP_PORT not set or null, set it's value to '5000'
+# then that value will be set to PORT
 PORT="${APP_PORT:=5000}"
 THREADS="${APP_THREADS:=1}"
 WORKERS="${APP_WORKERS:=1}"
