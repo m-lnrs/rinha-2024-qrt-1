f976ff370ba04a73aa825877b879b15fee1f7ce4
m-lnrs <matheuslinares@outlook.com>
2024-02-19 10:12:18 -0300

    Improve the help message

--- src/wsgi.py
+++ src/wsgi.py
@@ -18,7 +18,7 @@ except:
 if (__name__ == "__main__"):
 	parser = argparse.ArgumentParser(description='starts the flask application')
 	parser.add_argument('--dev', action='store_true', help='run the server on dev mode with debuger on')
-	parser.add_argument('--host', default='127.0.0.1', help='the host ip')
+	parser.add_argument('--host', default='127.0.0.1', help='the server host ip')
 	args, unknown = parser.parse_known_args()
 
 	app.config["DEBUG"] = args.dev
